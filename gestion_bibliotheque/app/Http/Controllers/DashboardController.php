00c272a07218078af7ba887630eee8771d7950db
khaoula khaldi <khaoula2417@gmail.com>
2026-04-11 11:18:13 +0100

    ajouter un controller de dashbord user qui contient une methode index qui retour mes achat et mes emprunt et aussi activiter de abonnment et les livre

---
+++ gestion_bibliotheque/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Livre;
+use App\Models\Achat;
+use App\Models\Emprunt;
+use App\Models\Subscription; 
+
+class DashboardController extends Controller{
+    public function index(){
+        $user = auth()->user();
+
+        $hasActiveSub = \App\Models\Subscription::where('user_id', $user->id)
+                                                ->where('statut', 'actif')
+                                                ->where('date_fin', '>', now())
+                                                ->exists();
+
+        if ($hasActiveSub) {
+            $livres = Livre::latest()->take(6)->get();
+        } else {
+            $livres = Livre::where('type', 'free')->latest()->take(6)->get();
+        }
+
+        $achats = $user->achats()->with('livre')->get();
+        $emprunts = $user->emprunts()->with('livre')->get();
+
+        return view('dashboard', compact(
+            'livres', 'achats', 'emprunts', 'hasActiveSub'
+        ));
+    }
+}
